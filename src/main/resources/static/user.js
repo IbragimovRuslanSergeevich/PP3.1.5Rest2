7155970b73b1048eed2c908e2e9742ab2aef8503
IbragimovRuslanSergeevich <rona88hh@gmail.com>
2023-11-24 23:54:18 +0300

    Добавил user.js в новом виде

--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -1,22 +1,45 @@
+/**
+ * @type {Object}
+ */
 let myUser = "";
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired");
 
-fetch("http://localhost:8080/user")
-    .then(res => res.json())
-    .then(data => {
-        myUser = data;
-        console.log(data)
-        showOneUser(myUser);
-    })
+    fetch("http://localhost:8080/user")
+        .then(res => res.json())
+        .then(data => {
+            myUser = data;
+            console.log(data)
+            showOneUser(myUser);
+            data.name
+            data.id
+            data.username
+            data.surname
 
-function showOneUser(user) {
-    let temp = "";
-    temp += "<tr>"
-    temp += "<td>" + user.id + "</td>"
-    temp += "<td>" + user.name + "</td>"
-    temp += "<td>" + user.surname + "</td>"
-    temp += "<td>" + user.age + "</td>"
-    temp += "<td>" + user.username + "</td>"
-    temp += "<td>" + user.roles + "</td>"
-    temp += "</tr>"
-    document.getElementById("oneUserBody").innerHTML = temp;
-}
+        })
+        .catch(error => console.error("Ошибка запроса:", error));
+
+
+    function showOneUser(user) {
+        let temp = "";
+        temp += "<tr>";
+        temp += "<td>" + (user.id !== undefined ? user.id : "") + "</td>";
+        temp += "<td>" + (user.name !== undefined ? user.name : "") + "</td>";
+        temp += "<td>" + (user.surname !== undefined ? user.surname : "") + "</td>";
+        temp += "<td>" + (user.age !== undefined ? user.age : "") + "</td>";
+        temp += "<td>" + (user.username !== undefined ? user.username : "") + "</td>";
+        temp += "<td>"; // начало блока для ролей
+
+        // Перебор ролей и добавление их в ячейку
+        if (user.roles !== undefined) {
+            user.roles.forEach(role => {
+                temp += role.roleType + " "; // предполагаем, что имя роли находится в свойстве roleType
+            });
+        }
+
+        temp += "</td>"; // конец блока для ролей
+        temp += "</tr>";
+
+        document.getElementById("oneUserBody").innerHTML = temp;
+    }
+});
